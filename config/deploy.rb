4830ff0957b56dfe426f558980e3f3f7cf383186
Naomi Dushay <ndushay@stanford.edu>
2017-02-21 15:50:22 -0800

    cap deploy now runs maven

--- config/deploy.rb
+++ config/deploy.rb
@@ -43,10 +43,11 @@ namespace :maven do
   desc 'package'
   task :package do
     on roles(:app), in: :sequence do
-      execute "cd #{current_path} && /usr/local/maven/bin/mvn clean package --settings /opt/app/ld4p/.m2/oracle-settings.xml"
+      execute "cd #{current_path} && /usr/local/maven/bin/mvn clean package"
     end
   end
 end
+after 'deploy:finished', 'maven:package'
 
 desc 'convert test file of one record'
 task :local_test do
